c83c9e1dc6029ec73aab78d727c62c106130a917
orangemug <info@orangemug.co.uk>
2018-09-05 18:22:21 +0100

    Fix to add preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,12 @@
 import CMS from 'netlify-cms'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
+import PricingPagePreview from './preview-templates/PricingPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
-CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('pricing', AboutPagePreview)
+CMS.registerPreviewTemplate('about', PricingPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
