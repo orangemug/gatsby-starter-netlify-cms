20ed55b85a5dd1b83d4d7cb328f1001df07e1ab4
orangemug <info@orangemug.co.uk>
2018-09-05 18:17:50 +0100

    Added pricing page preview template

---
+++ src/cms/preview-templates/PricingPagePreview.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { PricingPageTemplate } from '../../templates/about-page'
+
+const PricingPagePreview = ({ entry, widgetFor }) => (
+  <PricingPageTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+)
+
+PricingPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default PricingPagePreview
